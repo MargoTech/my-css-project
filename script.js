2ab20b041ec78f024678509d243fbec7726f375d
MargoTech <margobartashevich@gmail.com>
2025-01-15 10:59:58 +0100

    adding js

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Welcome to the Motivational Reads Hub!");
+
+document.querySelector("h1").textContent =
+  "Welcome to the Motivational Reads Hub!";
+
+document.querySelector("button").addEventListener("click", function () {
+  alert("Button clicked!");
+});
